5826b4a5406689015c6eebed5647fe7d9bf86a51
ESS Mac Pro <ESSMacPro@esss-macbook-pro.local>
2017-04-04 07:57:52 -0400

    Added Pan Gesture : 04/04/2017

--- MonkeyPinch/ViewController.swift
+++ MonkeyPinch/ViewController.swift
@@ -20,6 +20,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: self.view)
+        if let view = sender.view {
+            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        }
+        sender.setTranslation(CGPoint.zero, in: self.view)
+        
+        if sender.state == .ended {
+            //1
+            let velocity = sender.velocity(in: self.view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            let slideMultiplier = magnitude/200
+            print("magnitude: \(magnitude), slideMultiplier: \(slideMultiplier)")
+            
+            //2
+            let slideFactor = 0.1*slideMultiplier    //Increase for more of a slide
+            
+            //3
+            var finalPoint = CGPoint(x: (sender.view?.center.x)! + (velocity.x * slideFactor), y: (sender.view?.center.y)! + (velocity.y * slideFactor))
+            
+            //4
+            finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
+            finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+            
+            //5
+            UIView.animate(withDuration: Double(slideFactor*2), delay: 0, options: .curveEaseOut, animations: { 
+                sender.view?.center = finalPoint
+            }, completion: nil)
+        }
+        
+    }
 
 }
 
